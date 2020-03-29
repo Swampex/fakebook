8d76510b32f5a67d402db563b77a9e5dbcd40498
swampex@yandex.ru <KnMm45BS>
2020-03-29 20:58:38 +0300

    Updates

---
+++ precache-manifest.0866bfea78a029514da49abdcc00e173.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555d24714f6e5e4d1e0b367c1c1adc38",
+    "url": "/fakebook/index.html"
+  },
+  {
+    "revision": "c0414f68a7b9158d5151",
+    "url": "/fakebook/static/css/main.ef8d6072.chunk.css"
+  },
+  {
+    "revision": "97ba66c9ae3731e12638",
+    "url": "/fakebook/static/js/2.267f8b53.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fakebook/static/js/2.267f8b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62ce0d857dae432711c",
+    "url": "/fakebook/static/js/3.09eda4d1.chunk.js"
+  },
+  {
+    "revision": "f1e97f1a53832bb18024",
+    "url": "/fakebook/static/js/4.1c2a15fa.chunk.js"
+  },
+  {
+    "revision": "c0414f68a7b9158d5151",
+    "url": "/fakebook/static/js/main.ef0c7efe.chunk.js"
+  },
+  {
+    "revision": "a44b27b71e041159f727",
+    "url": "/fakebook/static/js/runtime-main.cb19b5dd.js"
+  },
+  {
+    "revision": "e2c610a5bb38d2f922b5635d93a9d4e3",
+    "url": "/fakebook/static/media/JohnDonne.e2c610a5.png"
+  },
+  {
+    "revision": "da5b1a73b4ef10adfe23cb168e3aa06c",
+    "url": "/fakebook/static/media/Petka.da5b1a73.png"
+  },
+  {
+    "revision": "1ae5e01b4f858171ecc7da4a6a5ef063",
+    "url": "/fakebook/static/media/anonymous.1ae5e01b.png"
+  },
+  {
+    "revision": "d59992713438d248048b172ceddc15b9",
+    "url": "/fakebook/static/media/football.d5999271.png"
+  },
+  {
+    "revision": "56620db277fbf849f314454eaba248bd",
+    "url": "/fakebook/static/media/monkey.56620db2.png"
+  },
+  {
+    "revision": "c3e9f6b140fe44ff8ba49f4ece139eaf",
+    "url": "/fakebook/static/media/school.c3e9f6b1.png"
+  }
+]);
